f6a34d389ec98e98dcc9b9174831c4ad3700a223
Dileep Kumar H G <dileep.kumarhg@hcl.com>
2022-05-15 13:08:56 +0530

    Dashboard api is created

---
+++ dashboardapi/src/main/java/com/hcl/dashboardapi/serviceimpl/DashboardService.java
@@ -0,0 +1,6 @@
+package com.hcl.dashboardapi.serviceimpl;
+
+public interface DashboardService {
+
+	public Long getAllTraining(String role,Long id);
+}
---
+++ dashboardapi/src/main/java/com/hcl/dashboardapi/serviceimpl/DashboardServiceImpl.java
@@ -0,0 +1,11 @@
+package com.hcl.dashboardapi.serviceimpl;
+
+public class DashboardServiceImpl implements DashboardService {
+
+	@Override
+	public Long getAllTraining(String role, Long id) {
+		// TODO Auto-generated method stub
+		return (long) 0;
+	}
+
+}
